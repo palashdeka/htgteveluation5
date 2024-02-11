d14cd1eb904701ddeb549981a785069f622a65a4
Palash Deka <palashjyoti12@gmail.com>
2024-02-11 21:46:11 +0530

    my
    git commit -m my_app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+import Header from "./components/Header";
+import Form from "./components/form";
+import TodosList from "./components/TodosList";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=()=>{
+
+  const [input,setInput]=useState("");
+  const[todos,setTodos]=useState([]);
+  const[editTodo,setEditTodo]= useState(null);
+  return <div className="container">
+    <div className="todo">
+      <Header/>
     </div>
-  );
+    <div>
+      <Form 
+      input={input}
+      setInput={setInput}
+      todos={todos}
+      setTodos={setTodos}
+      editTodo={editTodo}
+      setEditTodo={setEditTodo}
+      />
+    </div>
+    <div>
+      <TodosList todos={todos} setTodos={setTodos} 
+      setEditTodo={setEditTodo} />
+    </div>
+  </div>
 }
 
 export default App;
---
+++ src/components/form.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react'
+import{v4 as uuidv4} from "uuid"
+
+const Form=({input,setInput,todos,setTodos, editTodo, setEditTodo}) => {
+    const updateTodo = (title,id,completed) => {
+      const newTodo = todos.map((todo) => 
+        todo.id === id ? {title,id,completed} : todo
+      )
+      setTodos(newTodo);
+      setEditTodo("");
+    };
+    
+    const onInputChange=(event)=>{
+        setInput(event.target.value)
+    };
+    const onFormSubmit=(event)=>{
+        event.preventDefault();
+        if (!editTodo){
+          setTodos([...todos,{id:uuidv4(),title:input,competed:false}])
+        setInput("")
+        } else{
+          updateTodo(input,editTodo.id , editTodo.completed)
+        }
+        
+    }
+  return (
+    <form onSubmit={onFormSubmit}>
+      <input type="text" placeholder='Enter the details' className='task-input'
+      value={input} required onChange={onInputChange}/>
+      <button className='add-button' type='submit'>add</button>
+    </form>
+  )
+}
+export default Form;
